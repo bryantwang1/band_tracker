dd0deffc6ea13846827d82934dac48776f734a4a
Bryant Wang <bryant.m.wang@gmail.com>
2016-12-16 15:09:41 -0800

    Finish the conversion of cshtml files and add additional cshtml files along with proper HomeModule routing to fulfill the base functionality of the project.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -55,95 +55,87 @@ namespace BandTracker
                 return View["band.cshtml", selectedBand];
             };
 
-            // Get["/stylist/{id}/new_band"] = parameters => {
-            //     Stylist selectedStylist = Stylist.Find(parameters.id);
-            //     return View["specific_stylist_client_form.cshtml", selectedStylist];
-            // };
-            //
-            // Post["/stylist/{id}/new_client"] = parameters => {
-            //     Dictionary<string, object> model = new Dictionary<string, object> ();
-            //     Stylist selectedStylist = Stylist.Find(parameters.id);
-            //
-            //     string clientName = Request.Form["client-name"];
-            //     string clientDescription = Request.Form["client-description"];
-            //     int clientStylistId = selectedStylist.GetId();
-            //
-            //     Client newClient = new Client(clientName, clientDescription, clientStylistId);
-            //     newClient.Save();
-            //     model.Add("client", newClient);
-            //     model.Add("stylist", selectedStylist);
-            //     return View["client_added.cshtml", model];
-            // };
-            //
-            // Get["/stylist/edit/{id}"] = parameters => {
-            //     Stylist selectedStylist = Stylist.Find(parameters.id);
-            //     return View["stylist_edit.cshtml", selectedStylist];
-            // };
-            //
-            // Patch["/stylist/edit/{id}"] = parameters => {
-            //     Stylist selectedStylist = Stylist.Find(parameters.id);
-            //
-            //     string stylistName = Request.Form["stylist-name"];
-            //     string stylistPhone = Request.Form["stylist-phone"];
-            //     string stylistDescription = Request.Form["stylist-description"];
-            //
-            //     selectedStylist.Update(stylistName, stylistPhone, stylistDescription);
-            //     Stylist updatedStylist = Stylist.Find(parameters.id);
-            //     return View["stylist_edited.cshtml", updatedStylist];
-            // };
-            //
-            // Get["/client/edit/{id}"] = parameters => {
-            //     Dictionary<string, object> model = new Dictionary<string, object> ();
-            //     Client selectedClient = Client.Find(parameters.id);
-            //     Stylist selectedStylist = Stylist.Find(selectedClient.GetStylistId());
-            //     model.Add("client", selectedClient);
-            //     model.Add("all stylists", Stylist.GetAll());
-            //     return View["client_edit.cshtml", model];
-            // };
-            //
-            // Patch["/client/edit/{id}"] = parameters => {
-            //     Dictionary<string, object> model = new Dictionary<string, object> ();
-            //     Client selectedClient = Client.Find(parameters.id);
-            //
-            //     string clientName = Request.Form["client-name"];
-            //     string clientDescription = Request.Form["client-description"];
-            //     int clientStylistId = int.Parse(Request.Form["client-stylist-id"]);
-            //
-            //     selectedClient.Update(clientName, clientDescription, clientStylistId);
-            //     Client updatedClient = Client.Find(parameters.id);
-            //     Stylist selectedStylist = Stylist.Find(updatedClient.GetStylistId());
-            //
-            //     model.Add("client", updatedClient);
-            //     model.Add("stylist", selectedStylist);
-            //     return View["client_edited.cshtml", model];
-            // };
-            //
-            // Get["/stylist/delete/{id}"] = parameters => {
-            //     Stylist selectedStylist = Stylist.Find(parameters.id);
-            //     return View["stylist_delete.cshtml", selectedStylist];
-            // };
-            //
-            // Delete["/stylist/delete/{id}"] = parameters => {
-            //     Stylist selectedStylist = Stylist.Find(parameters.id);
-            //     selectedStylist.Delete();
-            //     return View["entry_deleted.cshtml"];
-            // };
-            //
-            // Get["/client/delete/{id}"] = parameters => {
-            //     Dictionary<string, object> model = new Dictionary<string, object> ();
-            //     Client selectedClient = Client.Find(parameters.id);
-            //     Stylist selectedStylist = Stylist.Find(selectedClient.GetStylistId());
-            //
-            //     model.Add("client", selectedClient);
-            //     model.Add("stylist", selectedStylist);
-            //     return View["client_delete.cshtml", model];
-            // };
-            //
-            // Delete["/client/delete/{id}"] = parameters => {
-            //     Client selectedClient = Client.Find(parameters.id);
-            //     selectedClient.Delete();
-            //     return View["entry_deleted.cshtml"];
-            // };
+            Get["/venue/{id}/new_band"] = parameters => {
+                Venue selectedVenue = Venue.Find(parameters.id);
+                return View["venue_add_band.cshtml", selectedVenue];
+            };
+
+            Post["/venue/{id}/new_band"] = parameters => {
+                Venue selectedVenue = Venue.Find(parameters.id);
+                int bandId = int.Parse(Request.Form["band-id"]);
+
+                Band selectedBand = Band.Find(bandId);
+                selectedVenue.AddBand(selectedBand);
+                return View["venue.cshtml", selectedVenue];
+            };
+
+            Get["/band/{id}/new_venue"] = parameters => {
+                Band selectedBand = Band.Find(parameters.id);
+                return View["band_add_venue.cshtml", selectedBand];
+            };
+
+            Post["/band/{id}/new_venue"] = parameters => {
+                Band selectedBand = Band.Find(parameters.id);
+                int venueId =int.Parse(Request.Form["venue-id"]);
+
+                Venue selectedVenue = Venue.Find(venueId);
+                selectedBand.AddVenue(selectedVenue);
+                return View["band.cshtml", selectedBand];
+            };
+
+            Get["/venue/edit/{id}"] = parameters => {
+                Venue selectedVenue = Venue.Find(parameters.id);
+                return View["venue_edit.cshtml", selectedVenue];
+            };
+
+            Patch["/venue/edit/{id}"] = parameters => {
+                Venue selectedVenue = Venue.Find(parameters.id);
+
+                string venueName = Request.Form["venue-name"];
+                string venueLocation = Request.Form["venue-location"];
+
+                selectedVenue.Update(venueName, venueLocation);
+                Venue updatedVenue = Venue.Find(parameters.id);
+                return View["venue_edited.cshtml", updatedVenue];
+            };
+
+            Get["/band/edit/{id}"] = parameters => {
+                Band selectedBand = Band.Find(parameters.id);
+                return View["band_edit.cshtml", selectedBand];
+            };
+
+            Patch["/band/edit/{id}"] = parameters => {
+                Band selectedBand = Band.Find(parameters.id);
+
+                string bandName = Request.Form["band-name"];
+                string bandDescription = Request.Form["band-description"];
+
+                selectedBand.Update(bandName, bandDescription);
+                Band updatedBand = Band.Find(parameters.id);
+                return View["band_edited.cshtml", updatedBand];
+            };
+
+            Get["/venue/delete/{id}"] = parameters => {
+                Venue selectedVenue = Venue.Find(parameters.id);
+                return View["venue_delete.cshtml", selectedVenue];
+            };
+
+            Delete["/venue/delete/{id}"] = parameters => {
+                Venue selectedVenue = Venue.Find(parameters.id);
+                selectedVenue.Delete();
+                return View["entry_deleted.cshtml"];
+            };
+
+            Get["/band/delete/{id}"] = parameters => {
+                Band selectedBand = Band.Find(parameters.id);
+                return View["band_delete.cshtml", selectedBand];
+            };
+
+            Delete["/band/delete/{id}"] = parameters => {
+                Band selectedBand = Band.Find(parameters.id);
+                selectedBand.Delete();
+                return View["entry_deleted.cshtml"];
+            };
         }
     }
 }
